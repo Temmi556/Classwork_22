1c8d9d0ce092e430d194df3236d9e9118235ef61
Temmik556 <temmi556@mail.ru>
2025-12-19 18:45:21 +0300

    Добавлены изменения

--- Classwork_22.c
+++ Classwork_22.c
@@ -6,14 +6,16 @@
 #define M_PI 3.14159265358979323846
 #define NUM_FUNCS 3
 
+// Указатель на функцию: объявление типа Func как указателя на функцию double(double)
 typedef double (*Func)(double);
 
+// Указатель на функцию: параметр f - указатель на функцию
 void tabulirovat(Func f, double a, double b, double h) {
     printf("--------------------------------------\n");
     printf("|     x     |   Результат функции   |\n");
     printf("--------------------------------------\n");
     for (double x = a; x <= b; x += h)
-        printf("|  %7.3lf  |  %18.6lf  |\n", x, f(x));
+        printf("|  %7.3lf  |  %18.6lf  |\n", x, f(x));  // Указатель на функцию: вызов функции через указатель f
     printf("--------------------------------------\n");
 }
 // Вариант 9 - Функции
@@ -35,7 +37,7 @@ double func3(double x) {
     return t + pow(t, 3) / 3 + pow(t, 5) / 5 + pow(t, 7) / 7;
 }
 
-// Массив указателей на функции
+// Указатель на функцию: массив указателей на функции
 Func funcs[NUM_FUNCS] = {func1, func2, func3};
 
 
@@ -52,17 +54,17 @@ void print_func_menu() {
     printf("Ваш выбор: ");
 }
 
-// Получение функции по выбору пользователя
+// Указатель на функцию: возвращает указатель на функцию из массива
 Func get_func(int choice) {
     if (choice >= 1 && choice <= NUM_FUNCS)
-        return funcs[choice - 1];
+        return funcs[choice - 1];  // Указатель на функцию: получение из массива по индексу
     return NULL;
 }
 // Главная функция
 int main() {
     int choice, func_choice;
     double x, a, b, h;
-    Func f;
+    Func f;  // Указатель на функцию: переменная для хранения указателя
     FILE *file;
 
     while (true) {
@@ -79,7 +81,7 @@ int main() {
         print_func_menu();
         scanf("%d", &func_choice);
         
-        f = get_func(func_choice);
+        f = get_func(func_choice);  // Указатель на функцию: присваивание указателя переменной f
         if (!f) {
             printf("Неверный выбор функции!\n");
             continue;
@@ -89,13 +91,13 @@ int main() {
             case 1:  // Вычислить значение
                 printf("Введите значение x: ");
                 scanf("%lf", &x);
-                printf("Результат: f(%.4lf) = %.6lf\n", x, f(x));
+                printf("Результат: f(%.4lf) = %.6lf\n", x, f(x));  // Указатель на функцию: вызов через указатель
                 break;
 
             case 2:  // Табулировать
                 printf("Введите a, b, h: ");
                 scanf("%lf %lf %lf", &a, &b, &h);
-                tabulirovat(f, a, b, h);
+                tabulirovat(f, a, b, h);  // Указатель на функцию: передача указателя в функцию
                 break;
 
             case 3:  // Запись в файл
@@ -103,7 +105,7 @@ int main() {
                 scanf("%lf %lf %lf", &a, &b, &h);
                 file = fopen("dat.txt", "w");
                 for (x = a; x <= b; x += h)
-                    fprintf(file, "%.4lf,%.6lf\n", x, f(x));
+                    fprintf(file, "%.4lf,%.6lf\n", x, f(x));  // Указатель на функцию: вызов через указатель
                 fclose(file);
                 printf("Данные успешно записаны в файл dat.txt\n");
                 break;
